f20d4cdd559c4190bd52b1f74d97f2ecc592620d
energy1234567 <113248425+energy1234567@users.noreply.github.com>
2022-11-24 17:22:35 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,462 @@
+// let gog = 5;
+// gog = 2;
+// gog;
+
+// console.log(gog)
+
+// let a=4;
+//     b=7;
+//     c=11;
+//     console.log(a,b,c)
+
+// let a = 4;
+// let b = 6;
+// c=9;
+// console.log(c)
+
+// war can = 5;
+// console.log(can)
+
+// var her = "ее";
+// console.log(her);
+
+// {
+//   let a = 5;
+//   console.log(a)
+// }
+// console.log(a)
+
+// const red = #000;
+// const blue = #135;
+
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+// console.log(blue)
+
+// переменная, в которой хранится выбранное математическое действие
+// функция расчёта
+// переменная для результата
+// получаем первое и второе число
+// смотрим, что было в переменной с действием, и действуем исходя из этого
+// отправляем результат на страницу
+
+// let admin, name;
+// name = "джон";
+// admin = name;
+//  alert( admin );
+
+// let ourPlanetName = "земля";
+// let ourPlanetName = "Земля";
+
+// let isBoss = confirm("Ты здесь главный?");
+
+// alert( isBoss ); // true, если нажата OK
+
+// let test = prompt("ваше имя?");
+
+// let name = prompt('как тебя зовут?');
+// alert (`тебя зовут  ${name} !`);
+
+// let username = prompt ('какой твой никнаме');
+// alert (`твой никнаме ${username} !`);
+
+// let x = 5;
+// x = -x;
+// alert(x);
+
+// let x=10, y=4;
+// alert ( x - y );
+
+// alert( 8 ** (1/3) );
+// let x = 10;
+// alert (-10);
+
+// alert ( +" ");
+// let apples=7;
+// let banana=10;
+// alert ( +apples + +banana );
+
+//  let x = 2  2 * 7;
+//  alert( x );
+
+// let a = 10;
+// let b = 20;
+// let c = 3 - (a = b + 2);
+// alert (a);
+// alert (c);
+
+// let n = 10 , a = 2;
+// a /= 2;
+// alert (a);
+
+// let n = 11;
+
+// n *= 4 + 7;
+// alert (n); 121
+
+// let counter = -7;
+//  let a = counter++;
+// alert(a);
+
+// let a=0;
+// b =
+// --a
+// +
+// a--
+// +
+// ++a
+// -
+// a++;
+// b
+
+// let a = 1 ;
+// let c = 2;
+
+// b = a++ - --a + --c + c++ * ++c + a-- / ++c;
+
+// let a = 0;
+// let b = false;
+// console.log(a === b);
+
+// let name = prompt("Кто там?", '');
+
+// let name = prompt("Кто там?", '');
+
+// if (name === 'admin') {
+
+//   let pass = prompt('Пароль?', '');
+//   if (pass === '12345') {
+//      alert( 'e salom uzbellaga!' );
+//    } else if (pass === '' || pass === null) {
+//      alert( 'otmena ku yana, sassiq' );
+//    } else {
+//      alert( 'fu' );
+//    }
+
+//  } else if (name === '' || name === null) {
+//    alert( 'otmen buldi' );
+//  } else {
+//    alert( "admin mas san, toba" );
+//  }
+
+// let x = 2  2 * 7;
+//   alert( x );
+
+// if ( age < 5) {
+// message = 'hello';
+// }else if (age < 20){
+// message = 'привет ';
+// } else if (age < 80){
+// message = 'здраствуйте'}
+
+// else {
+//   message =
+// }
+
+// let a = 2;
+// let x = 5 + (a *= 2 + (a += 4) + a++);
+// alert (a)
+
+//  alert ("" - 1 + 0 );
+
+// let name = prompt(" введи логин", '');
+
+// if (name === 'admin') {
+
+//   let pass = prompt('Пароль?', '');
+
+//   if (pass === '12345') {
+//     alert( 'e salom uzbellaga!' );
+//   } else if (pass === '' || pass === null) {
+//     alert( 'otmena ku yana, sassiq' );
+//   } else {
+//     alert( 'fu' );
+//   }
+
+// } else if (name === '' || name === null) {
+//   alert( 'otmen buldi' );
+// } else {
+//   alert( "admin mas san, toba" );
+// }
+
+// let whoIsIt;
+
+// let prompt ("cal");
+
+// // функция расчёта
+// function func() {
+//   // переменная для результата
+//   let result;
+//   // получаем первое и второе число
+//   let num1 = Number(document.getElementById("num1").value);
+//   let num2 = Number(document.getElementById("num2").value);
+//   // смотрим, что было в переменной с действием, и действуем исходя из этого
+//   switch (cal) {
+//     case '+':
+//       // console.log();
+//       result = num1 + num2;
+//       break;
+//     case '-':
+//       result = num1 - num2;
+//       break;
+//     case '*':
+//       result = num1 * num2;
+//       break;
+//     case '/':
+//       result = num1 / num2;
+//       break;
+//   }
+
+// }
+
+// let letter = "b";
+
+// switch (letter) {
+// 	case "a":
+// 		console.log("Эй эй лалалей");
+// 		break;
+// 	case "b":
+// 		console.log("Биполярка");
+// 		break;
+// 	case "c":
+// 		console.log("Саня хуй саси");
+// 		break;
+// 	case "d":
+// 		console.log("Нахуй сам иди");
+// 		break;
+// 	case "e":
+// 		console.log("ИИе тоба");
+// 		break;
+// 	default:
+//         console.log("че то другое");
+// 		break;
+// }
+
+//   let Number = prompt("Число" , '') ;
+
+//   if ( Number > 20) {
+//    alert( 'Викинги убивают детей' ); }
+//   // else if (Number > 101) {
+//   //  alert( 'викинги не убивают детей' );}
+//    else  (Number  < 19);{
+//      alert ( 'викинги не убивают детей');
+//  }
+
+// let name = prompt(" введи логин", '');
+
+// if (name === 'admin') {
+
+//   let pass = prompt('Пароль?', '');
+
+//   if (pass === '12345') {
+//     alert( 'e salom uzbellaga!' );
+//   } else if (pass === '' || pass === null) {
+//     alert( 'otmena ku yana, sassiq' );
+//   } else {
+//     alert( 'fu' );
+//   }
+
+// } else if (name === '' || name === null) {
+//   alert( 'otmen buldi' );
+// } else {
+//   alert( "admin mas san, toba" );
+//
+
+// year = prompt("Количество лет");
+// month = year * 12;
+// let deadKids = 0;
+
+// for (let i = 0; i < month; i++) {
+//   let humans = Math.ceil(Math.random() * 100);
+//   let kids = (humans / 100) * 20;
+//   kids = Math.ceil(kids);
+//   if (kids < 15) {
+//     // console.log(kids, humans)
+//   } else {
+//     deadKids += kids;
+//   }
+// }
+// console.log(deadKids)
+
+// for (let i = 0; i < array.length; i++) {
+//   const element = array[i];
+  
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Output = document.querySelector("#first")
+// let lines = prompt("Сколько звезд");
+// let string = "";
+
+// for ( let i = 0; i < lines; i++){
+//    string += "+"
+//    Output.innerHTML += string + "<br>"
+// }
+
+// console.log(Output);
+
+// const output2 = document.querySelector(".second");
+// let lines2 = prompt("Сколько звезд");
+// let string2 = "";
+
+// let count = lines2;
+// for (let i = 0; i < lines2 ; i++) {
+//    string2 = ""
+//    for (let j = 0; j < count; j++) {
+//     string2 += "+"
+
+
+//    }
+//    count--
+// output2.innerHTML += string2 + "<br>"
+
+// }
+
+// const output3 = document.querySelector(".third");
+// let lines3 = prompt("qvadrat");
+// for (let i = 0; i < lines3; i++) {
+//     let string3 = "";
+
+//     for (let j = 0; j < lines3; j++) {
+//         string3 += "+"
+
+//     }
+//     output3.innerHTML += string3 + "<br>"
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function showMessage(a) {
+//   a = prompt ('P squarre' ,'4' );
+//   console.log( 4 * a );
+// }
+
+// showMessage();
+
+
+// function showName (b) {
+//   b = prompt ('S square','8');
+//   console.log(b*b) 
+  
+// }
+// showName(b);
+
+
+// function showAame(c){
+//   c = prompt ('перим.прям' , '7')
+//   d = prompt ( 'перим.прям' , '10')
+//   console.log(c*d )
+// }
+// showAame(c)
+
+
+
+
+
+// function max(a, b) { 
+//   if(a >= b) 
+//     return a; 
+//   else
+//     return b; 
+//     console.log (max (0, 10));
+// }
+
+
+
+
+function min(a, b) { // это первое 
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+
+
+
+
+// это второй
+function pow(x, n) {  
+  let result = x;
+
+  for (let i = 1; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+let x = prompt("x?", '');
+let n = prompt("n?", '');
+
+if (n < 1) {
+  alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+} else {
+  alert( pow(x, n) );
+}
+
+
+
+
+
+// задание 3
+for (let i = 1; i <= 101; i++) {
+  if (i % 2 !== 0) {
+    alert( i );
+  }
+}
